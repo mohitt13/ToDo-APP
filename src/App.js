33cd28954912651cbeb4e7f89666cd36103fa7a4
mohitt13 <49208187+mohitt13@users.noreply.github.com>
2020-05-26 17:21:46 +0530

    001 | Api and simple html page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form action={"http://localhost:3001"} method={"POST"}>
+        <input id={"task"} name={"task"} placeholder={"Task"} className={"text_area"}/>
+         <button className="btn" type={"submit"}>Add</button><br/>
+     </form>
     </div>
   );
 }
